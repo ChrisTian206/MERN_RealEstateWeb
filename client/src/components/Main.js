bb70733ee292a395b0bda911d218c23fd92a5ad4
Wenhao Tian <wtian16@bu.edu>
2023-10-15 20:00:48 -0400

    conditionally show login and logout

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -19,27 +19,45 @@ export default function Main() {
         toast.success('You have logged out')
     }
 
+    //Check if a user is considered 'logged in' by verifying if the 'auth' object is not null, 
+    //the 'user' property is not null, and both the 'token' and 'refreshToken' properties are present and truthy.
+    /** Another way to write this 
+     * let isLogin = false;
+     * 
+    if (auth && auth.user !== null && auth.token && auth.refreshToken) {
+        isLogin = true;
+    }
+     */
+    const isLogin = auth?.user !== null && auth.token && auth.refreshToken
+
     return (
         <nav className="nav d-flex justify-content-between p-2 lead">
             <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
-            <NavLink className="nav-link" to="/login">Login</NavLink>
-            <NavLink className="nav-link" to="/register">Register</NavLink>
 
-            <div className="dropdown">
-                <li>
-                    <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                        User
-                    </a>
-                    <ul className="dropdown-menu">
-                        <li>
-                            <NavLink className="nav-link" to="/dashboard">Dashboard</NavLink>
-                        </li>
-                        <li>
-                            <a className="nav-link" onClick={handleLogout} style={{ cursor: 'pointer' }}>Log out</a>
-                        </li>
-                    </ul>
-                </li>
-            </div>
+            {!isLogin ? (
+                <>
+                    <NavLink className="nav-link" to="/login">Login</NavLink>
+                    <NavLink className="nav-link" to="/register">Register</NavLink>
+                </>
+            ) : ('')}
+
+            {!isLogin ? ('') : (
+                <div className="dropdown">
+                    <li>
+                        <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                            {auth?.user?.name ? auth.user.name : auth.user.username}
+                        </a>
+                        <ul className="dropdown-menu">
+                            <li>
+                                <NavLink className="nav-link" to="/dashboard">Dashboard</NavLink>
+                            </li>
+                            <li>
+                                <a className="nav-link" onClick={handleLogout} style={{ cursor: 'pointer' }}>Log out</a>
+                            </li>
+                        </ul>
+                    </li>
+                </div>
+            )}
         </nav>
     )
 }
