f269a6318157c8c5882675c629733a7b91cb0e80
Wenhao Tian <wtian16@bu.edu>
2023-10-29 00:10:56 -0400

    update createAd

--- client/src/pages/user/Ad/CreateAd.js
+++ client/src/pages/user/Ad/CreateAd.js
@@ -7,6 +7,16 @@ export default function CreateAd() {
     const [rent, setRent] = useState(false);
 
     const navigate = useNavigate();
+
+    const handleSell = () => {
+        setRent(false);
+        setSell(true);
+    }
+
+    const handleRent = () => {
+        setSell(false);
+        setRent(true);
+    }
     return (
         <div>
             <h1 className="display-1 bg-primary text-light p-5"> Create Ad </h1>
@@ -14,11 +24,13 @@ export default function CreateAd() {
             <div className="d-flex justify-content-center align-items-center vh-100"
                 style={{ marginTop: '-8%' }}>
                 <div className="col-lg-6">
-                    <button>Sell</button>
+                    <button onClick={handleSell} className="btn btn-light btn-lg col-12 p-5">Sell</button>
+                    {sell && "Sell: House and Land options"}
                 </div>
 
                 <div className="col-lg-6">
-                    <button>Rent</button>
+                    <button onClick={handleRent} className="btn btn-light btn-lg col-12 p-5">Rent</button>
+                    {rent && "Rent: house and land options"}
                 </div>
             </div>
         </div>
