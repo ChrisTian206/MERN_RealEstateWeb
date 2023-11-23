6ad5f93ee67a16249db895e2e887a374d2effcd8
Wenhao Tian <wtian16@bu.edu>
2023-11-22 19:29:56 -0500

    addForm and import Google place API

--- client/src/components/form/AdForm.js
+++ client/src/components/form/AdForm.js
@@ -1,8 +1,37 @@
+import { useState } from "react"
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import { GOOGLE_PLACES_KEY } from "../../config";
+
 export default function AdForm({ action, type }) {
 
+    const [ad, setAd] = useState({
+        photos: [],
+        uploading: false,
+        price: '',
+        address: '',
+        bedrooms: '',
+        bathrooms: '',
+        parking: '',
+        landSize: '',
+        type: '',
+        title: '',
+        description: '',
+        loading: false,
+    });
     return (
         <>
-            <h1>This is ad form for {action} / {type}</h1>
+            <div className="mb-3 form-control">
+                <GooglePlacesAutocomplete
+                    apiKey={GOOGLE_PLACES_KEY}
+                    apiOptions='us'
+                    selectProps={{
+                        defaultInputValue: ad?.address,
+                        placeholder: "Search for address...",
+                        onChange: (address) => console.log(address)
+                    }} />
+
+                <button className="btn btn-primary btn-lg col-12 mt-3">Submit</button>
+            </div>
         </>
     )
 }
