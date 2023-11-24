f7698cddbe9ad6caa6a01acd2cd2d5d39c1ae797
Wenhao Tian <wtian16@bu.edu>
2023-11-23 20:55:02 -0500

    image sent and resize

--- client/src/components/form/UploadImage.js
+++ client/src/components/form/UploadImage.js
@@ -1,3 +1,5 @@
+import resizer from 'react-image-file-resizer';
+
 export default function UploadImage({ ad, setAd }) {
 
     const handleUploadImages = async (e) => {
@@ -5,8 +7,32 @@ export default function UploadImage({ ad, setAd }) {
             let files = e.target.files;
             files = [...files];
             if (files?.length) {
-                console.log(files);
                 setAd({ ...ad, uploading: true })
+
+                files.map(img => {
+                    new Promise(() => {
+                        resizer.imageFileResizer(
+                            img,   //file
+                            1080, //width
+                            720, //height
+                            'JPEG', //format
+                            100, //quality
+                            0, //rotation
+                            async (uri) => { //callback
+                                try {
+                                    let { data } = await axios.post(`/upload-images`, { image: uri });
+                                    //image is sent to the back-end in base 64 format.
+                                    setAd({ ...ad, photos: [...ad.photos, data], uploading: false })
+                                } catch (err) {
+                                    console.log(err);
+                                    setAd({ ...ad, uploading: false })
+                                }
+                            },
+                            'base64' //outputType, other options are 'blob', 'file', 'uri', 'base64'
+                        );
+                    }
+                    )
+                })
             }
         } catch (err) {
             console.log(err)
